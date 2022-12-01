422b98338a88cac824dd0977254e85450d8543da
Steven <steven.camillieri@protonmail.com>
2022-12-01 09:13:43 -0500

    setting up mongoose

---
+++ server.js
@@ -0,0 +1,54 @@
+require("dotenv").config();
+const errorHandler = require("./middleware/errorHandler");
+const connectDB = require("./config/dbConn");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const app = express();
+const corsOptions = require("./config/corsOptions");
+const { logger, logEvents } = require("./middleware/logger");
+const PORT = process.env.PORT || 3500;
+
+console.log(process.env.NODE_ENV);
+
+connectDB();
+
+app.use(logger);
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use("/", express.static(path.join(__dirname, "public")));
+
+app.use("/", require("./routes/root"));
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 not found" });
+  } else {
+    res.type("txt").send("404 not found");
+  }
+});
+
+app.use(errorHandler);
+
+mongoose.connection.once("open", () => {
+  console.log("connected to mongoDB");
+  app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    `mongoErrLog.log`
+  );
+});
